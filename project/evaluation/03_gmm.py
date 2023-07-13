8dafc1999730c452a933d9e6ef1cabe9d46ec845
Filippo Greco s309529 <s309529@studenti.polito.it>
2023-07-13 19:37:18 +0200

    evaluation

---
+++ project/evaluation/03_gmm.py
@@ -0,0 +1,34 @@
+
+from mllib import *
+
+if __name__ == '__main__':
+    DTR, LTR = load('../Train.txt')
+    DTE, LTE = load('../Test.txt')
+
+    P = PCA_directions(DTR, 7)
+    DTR = np.dot(P.T, DTR)
+    DTE = np.dot(P.T, DTE)
+
+    Kc1 = [1, 2]
+    Kc0 = [4, 8]
+ 
+    # effective prior
+    p = 1/11
+
+    for Kc1i in Kc1:
+        for Kc0i in Kc0:
+            DTR0 = DTR[:, LTR == 0]
+            DTR1 = DTR[:, LTR == 1]
+
+            gmm0, _ = LBG_wrap(DTR0, n_iter=int(np.log2(Kc0i) + 1))
+            gmm1, _ = LBG_wrap(DTR1, n_iter=int(np.log2(Kc1i) + 1))
+
+            S0 = logpdf_GMM(DTE, gmm0)
+            S1 = logpdf_GMM(DTE, gmm1)
+
+            S = S1 - S0
+
+            print(f'for Kc1={Kc1i}, Kc0={Kc0i}')
+            print(DCF_min(p, 1, 1, S, LTE))
+    
+    # print(DCF_actual(p, 1, 1, S, LTE))
