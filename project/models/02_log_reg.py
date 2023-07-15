bb516a6a13b7418efea32e52f82dd51983e265d1
Filippo Greco s309529 <s309529@studenti.polito.it>
2023-07-15 15:36:36 +0200

    log-reg results updated

--- project/models/02_log_reg.py
+++ project/models/02_log_reg.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
 
     Dc = centering(DPCA7)
     Ds = std_variances(Dc)
-    # Dw = whitening(Ds, DPCA7)
-    # Dl = l2(Dw)
-    expD = expand_feature_space(Ds)
+    Dw = whitening(Ds, DPCA7)
+    Dl = l2(Dw)
+    expD = expand_feature_space(Dl)
 
     # folds
     K = 10
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
             # use maxfun=[>1500], maxiter[>30], factr=[<10**7] to increment precision
             x0 = np.zeros(DTR.shape[0] + 1)
-            x, f, d = sp.optimize.fmin_l_bfgs_b(logreg_obj_weight_wrap(DTR, LTR, li, 1/11), x0)
+            x, f, d = sp.optimize.fmin_l_bfgs_b(logreg_obj_weight_wrap(DTR, LTR, li, 0.9), x0)
 
             w, b = x[0:-1], x[-1]
             S = np.dot(w, DTE) + b
